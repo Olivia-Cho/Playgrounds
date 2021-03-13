11a51b2608d123a4bf2224f29108cda625162e19
Olivia-Cho <77197902+Olivia-Cho@users.noreply.github.com>
2021-03-13 08:40:43 -0500

    Update 1-Initializing Your Expert.swift

--- Code 2/3-Initialization/1-Initializing Your Expert.swift
+++ Code 2/3-Initialization/1-Initializing Your Expert.swift
@@ -1,3 +1,44 @@
 
 let expert = Expert()
 expert.turnLockUp()
+for i in 1 ... 2 {
+    expert.turnLeft()
+    expert.moveForward()
+    expert.moveForward()
+    expert.moveForward()
+    expert.collectGem()
+    expert.turnLeft()
+}
+for i in 1 ... 2 {
+    expert.turnLeft()
+    expert.turnLeft()
+    expert.moveForward()
+    expert.moveForward()
+    expert.moveForward()
+    expert.collectGem()
+}
+expert.turnRight()
+expert.moveForward()
+expert.moveForward()
+expert.moveForward()
+
+expert.turnLeft()
+expert.moveForward()
+expert.moveForward()
+expert.moveForward()
+expert.turnLeft()
+expert.moveForward()
+expert.turnRight()
+expert.turnLockUp()
+
+expert.turnRight()
+expert.moveForward()
+expert.turnRight()
+expert.moveForward()
+expert.moveForward()
+expert.moveForward()
+expert.turnLeft()
+expert.moveForward()
+expert.moveForward()
+expert.collectGem()
+
