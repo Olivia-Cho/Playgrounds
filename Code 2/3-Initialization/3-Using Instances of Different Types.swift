6d93e254f4ca2eb5ea74e06aa4af2cc25ea11d25
Olivia-Cho <77197902+Olivia-Cho@users.noreply.github.com>
2021-03-27 08:25:42 -0400

    Update 3-Using Instances of Different Types.swift
    Completed

--- Code 2/3-Initialization/3-Using Instances of Different Types.swift
+++ Code 2/3-Initialization/3-Using Instances of Different Types.swift
@@ -1,1 +1,22 @@
+let expert = Expert()
+let character = Character()
 
+expert.moveForward()
+expert.turnLockUp()
+
+func MoveCollect() {
+    character.moveForward()
+    character.collectGem()
+}
+func move2times() {
+    for i in 1 ... 2{
+        character.moveForward()
+    }
+}
+MoveCollect()
+character.moveForward()
+character.turnRight() 
+move2times()
+expert.turnLockDown()
+expert.turnLockDown()
+MoveCollect()
