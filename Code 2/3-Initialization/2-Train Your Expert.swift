db188f42de7b98a5254b85a3c2fc83d8266642ac
Olivia-Cho <77197902+Olivia-Cho@users.noreply.github.com>
2021-03-27 08:31:51 -0400

    completed

--- Code 2/3-Initialization/2-Train Your Expert.swift
+++ Code 2/3-Initialization/2-Train Your Expert.swift
@@ -1,39 +1,3 @@
-let expert = Expert()
-
-func turnTheLockDown() {
-    moveForward2Times()
-    expert.turnLockDown()
-}
-func moveForward2Times() {
-    expert.moveForward()
-    expert.moveForward()
-}
-
-func turnAround() {
-    expert.turnLeft()
-    expert.turnLeft()
-}
-func crossTheLineAndCollectGem() {
-    if expert.isOnGem{
-        expert.collectGem()
-    }
-    if expert.isBlocked{
-        turnAround()
-    }
-    moveForward2Times()
-    moveForward2Times()
-    
-}
-
-turnAround()
-turnTheLockDown()
-crossTheLineAndCollectGem()
-
-
-
-
-//This is the completed version
-
 func moveForward2Times() {
     for i in 1 ... 2{
         expert.moveForward()
