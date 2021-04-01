00426432fafe97af3919aa80c30de91bcc241ac2
Olivia-Cho <77197902+Olivia-Cho@users.noreply.github.com>
2021-03-31 20:04:42 -0400

    Update 4-It Takes Two.swift

--- Code 2/3-Initialization/4-It Takes Two.swift
+++ Code 2/3-Initialization/4-It Takes Two.swift
@@ -1,1 +1,31 @@
+let expert = Expert()
+let character = Character()
+func move3Times() {
+    for i in 1 ... 3 {
+        expert.moveForward()
+    }
+}
+func moveForward2times() {
+    for i in 1 ... 2 {
+        character.moveForward()
+    }
+}
+func turnAround() {
+    expert.turnRight()
+    expert.turnRight()
+}
+
+expert.turnLeft()
+move3Times()
+expert.turnRight()
+moveForward2times()
+expert.turnLeft()
+    // I left off here where I'm supposed to turn the purple lock down
+expert.turnLockDown()
+expert.turnLockDown()
+moveForward2times()
+character.collectGem()
+expert.turnLeft()
+expert.moveForward()
+expert.moveForward()
 
