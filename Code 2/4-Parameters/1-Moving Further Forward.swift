00b3522c3882eaa5d0049ca9e3daa2fa148b8806
Olivia-Cho <77197902+Olivia-Cho@users.noreply.github.com>
2021-04-10 08:32:22 -0400

    Update 1-Moving Further Forward.swift

--- Code 2/4-Parameters/1-Moving Further Forward.swift
+++ Code 2/4-Parameters/1-Moving Further Forward.swift
@@ -1,6 +1,37 @@
 
 let expert = Expert()
+
+func move(distance: int)
 for i in 1 ... 5{
         expert.moveForward()
     }
-move(distance: )
+
+func move5timesandTurnLeft() {
+    for i in 1 ... 2 {
+        move(distance: 5)
+        expert.turnLeft()
+    }
+}
+func move3TimesandTurnRight() {
+    for i in 1 ... 3 {
+        expert.moveForward()
+    }
+    expert.turnRight()
+}
+func moveForwardAndTurnRight() {
+    expert.moveForward()
+    expert.turnRight()
+}
+move(distance: 5)
+moveForwardAndTurnRight()
+expert.moveForward()
+moveForwardAndTurnRight()
+move5timesandTurnLeft()
+expert.turnLockUp()
+expert.turnLeft()
+move3TimesandTurnRight()
+move3TimesandTurnRight()
+for i in 1 ... 4 {
+    expert.moveForward()
+}
+expert.collectGem()
