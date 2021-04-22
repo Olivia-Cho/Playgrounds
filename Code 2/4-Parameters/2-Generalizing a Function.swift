df3e6013a07e00c92445b19a29c8a8dd3c1bebf0
Olivia-Cho <77197902+Olivia-Cho@users.noreply.github.com>
2021-04-21 20:00:15 -0400

    Update 2-Generalizing a Function.swift

--- Code 2/4-Parameters/2-Generalizing a Function.swift
+++ Code 2/4-Parameters/2-Generalizing a Function.swift
@@ -6,13 +6,39 @@ func turnLock(up: Bool, numberOfTimes : Int) {
         expert.turnLockUp()
     }
     
-  turnLock(up: true, numberOfTimes: 3)
-expert.turnRight()
-expert.turnRight()
+  func CharacterturnAround() {
+    character.turnLeft()
+    character.turnLeft()
+}
+func ExpertturnAround(){
+    expert.turnRight()
+    expert.turnRight()
+}
+func step3Times() {
+    for i in 1 ... 3 {
+        character.moveForward()
+    }
+}
+
 turnLock(up: true, numberOfTimes: 3)
+ExpertturnAround()
+turnLock(up: true, numberOfTimes: 3)
+step3Times()
+character.collectGem()
+CharacterturnAround()
 character.moveForward()
+character.turnLeft()
 character.moveForward()
+character.turnLeft()
+expert.turnLockDown()
+expert.turnLockDown()
+ExpertturnAround()
+expert.turnLockDown()
+expert.turnLockDown()
 character.moveForward()
 character.collectGem()
-
-  
+CharacterturnAround()
+character.moveForward()
+character.moveForward()
+ExpertturnAround()
+expert.turnLockDown()
