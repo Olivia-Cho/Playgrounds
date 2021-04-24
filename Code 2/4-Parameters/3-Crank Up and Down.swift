ec18158e7fd1cf9c6ca6705e3d72110afd9cd376
Olivia-Cho <77197902+Olivia-Cho@users.noreply.github.com>
2021-04-24 11:08:28 -0400

    Update 3-Crank Up and Down.swift

--- Code 2/4-Parameters/3-Crank Up and Down.swift
+++ Code 2/4-Parameters/3-Crank Up and Down.swift
@@ -1,1 +1,7 @@
-
+expert = Expert()
+character = Character()
+/* Expert needs to activate all the locks, so they can lift up the platforms and character can cross to collect the gems.
+ Note: A gem is always next to a platform.
+ Then Character needs to walk and collect gems while expert turns the platforms up.
+ Note: there is a pattern where Character needs to go on the platform, go back down, then collect the gem. Next go back up moveforward and collect the gem, turn left and repeat.
+ */
