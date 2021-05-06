e8b9714ad77f14cb2d0494b0e27a25a41cec6864
Olivia-Cho <77197902+Olivia-Cho@users.noreply.github.com>
2021-05-05 20:21:44 -0400

    Update 3-Crank Up and Down.swift

--- Code 2/4-Parameters/3-Crank Up and Down.swift
+++ Code 2/4-Parameters/3-Crank Up and Down.swift
@@ -1,9 +1,4 @@
-func collectAgemAtTheBottem() {
-    character.moveForward()
-    expert.turnLockDown()
-    expert.turnLockDown()
-    expert.turnLockDown()
-}
+
 func go3steps() {
     for i in 1 ... 3 {
         character.moveForward()
@@ -12,6 +7,17 @@ func go3steps() {
 func CharacterturnAround() {
     character.turnRight()
     character.turnRight()
+}
+func collectTheGemAtTheBottem() {
+    expert.turnLockUp()
+    character.turnRight()
+    character.moveForward()
+    character.moveForward()
+    character.collectGem()
+}
+func moveForward2Times() {
+    character.moveForward()
+    character.moveForward()
 }
 
 character.move(distance: 2)
@@ -32,12 +38,37 @@ character.move(distance: 2)
 character.collectGem()
 CharacterturnAround()
 character.moveForward()
-expert.turnLockDown()
-expert.turnLockDown()
-expert.turnLockDown()
+for i in 1 ... 3 {
+    expert.turnLockDown()
+}
 character.turnLeft()
 character.moveForward()
 character.collectGem()
 CharacterturnAround()
 character.move(distance: 2)
-
+expert.turnLeft()
+collectTheGemAtTheBottem()
+CharacterturnAround()
+character.move(distance: 2)
+character.turnRight()
+character.move(distance: 2)
+character.turnRight()
+expert.turnLeft()
+expert.turnLockUp()
+character.move(distance: 2)
+character.collectGem()
+CharacterturnAround()
+character.move(distance: 2)
+character.turnRight()
+expert.turnLeft()
+expert.turnLockUp()
+character.move(distance: 2)
+character.collectGem()
+CharacterturnAround()
+character.moveForward()
+expert.turnLockUp()
+expert.turnLockUp()
+expert.turnLockUp()
+character.turnLeft()
+character.moveForward()
+character.collectGem()
