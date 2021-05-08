9c68efd61fb2b34fae2cd05767c2937285601940
Olivia-Cho <77197902+Olivia-Cho@users.noreply.github.com>
2021-05-08 08:19:46 -0400

    completed

--- Code 2/4-Parameters/4-Placing at a Specific Location.swift
+++ Code 2/4-Parameters/4-Placing at a Specific Location.swift
@@ -1,1 +1,45 @@
 
+let expert = Expert()
+world.place(expert, atColumn: 2, row: 6)
+
+    /* Psudo Code
+ turnRight and move Forward
+ collect the gem
+ turnleft and pull the lock up once
+ move 5 times
+ turnleft and bring the lock up
+ turnRight and moveforward thenn collect gem
+ turn left
+ move
+ turnleft
+ move
+ move 5 times
+ collect
+ */
+
+func turnAround() {
+    expert.turnLeft()
+    expert.turnLeft()
+}
+func move5times() {
+    for i in 1 ... 5 {
+        expert.moveForward()
+    }
+}
+func bringTheSwitchUpThenCollectTheGem() {
+    expert.turnRight()
+    expert.moveForward()
+    expert.collectGem()
+    turnAround()
+    expert.moveForward()
+    expert.turnLockUp()
+    expert.turnRight()
+}
+
+
+bringTheSwitchUpThenCollectTheGem()
+move5times()
+bringTheSwitchUpThenCollectTheGem()
+expert.moveForward()
+move5times()
+expert.collectGem()
