98188313ddc89bc0effdea44205f778b13e30456
Olivia-Cho <77197902+Olivia-Cho@users.noreply.github.com>
2021-05-12 20:17:26 -0400

    completed

--- Code 2/4-Parameters/5-Rivers to Cross.swift
+++ Code 2/4-Parameters/5-Rivers to Cross.swift
@@ -1,1 +1,46 @@
 
+/* 1, 8 Psudo Code
+ collect gem
+ move 3 times
+ turnLockdown
+ turnLeft
+ move forward 4 times
+ collect
+ collect
+ turnLockUP
+ turnRight
+ move and collect
+ move 3 times
+ */
+let expert = Expert()
+world.place(expert, facing: south, atColumn: 1, row: 8)
+func move3times() {
+    for i in 1 ... 3{
+        expert.moveForward()
+    }
+}
+func moveAndCollect() {
+    expert.moveForward()
+    expert.collectGem()
+}
+func move4times() {
+    for i in 1 ... 4 {
+        expert.moveForward()
+    }
+}
+
+expert.collectGem()
+for i in 1 ... 3{
+    moveAndCollect()
+}
+expert.turnLockDown()
+expert.turnLeft()
+move3times()
+expert.collectGem()
+moveAndCollect()
+expert.turnLockUp()
+expert.turnRight()
+moveAndCollect()
+expert.move(distance: 2)
+expert.collectGem()
+moveAndCollect()
